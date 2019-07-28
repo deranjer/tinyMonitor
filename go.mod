ddbe4fef65648fb0acb6633ddb485f37ad3b3402
Jared Swets <deranjer@gmail.com>
2019-07-28 14:02:49 -0400

    fixing encoding and decoding

--- go.mod
+++ go.mod
@@ -3,7 +3,10 @@ module github.com/deranjer/tinyMonitor
 go 1.12
 
 require (
+	github.com/DataDog/zstd v1.4.1 // indirect
+	github.com/Sereal/Sereal v0.0.0-20190618215532-0b8ac451a863 // indirect
 	github.com/asdine/storm v2.1.2+incompatible
+	github.com/golang/snappy v0.0.1 // indirect
 	github.com/magiconair/properties v1.8.1 // indirect
 	github.com/pelletier/go-toml v1.4.0 // indirect
 	github.com/rs/zerolog v1.14.3
@@ -11,6 +14,7 @@ require (
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/viper v1.4.0
 	github.com/stretchr/testify v1.3.0 // indirect
+	github.com/vmihailenco/msgpack v4.0.4+incompatible // indirect
 	golang.org/x/sys v0.0.0-20190712062909-fae7ac547cb7 // indirect
 	golang.org/x/text v0.3.2 // indirect
 	nanomsg.org/go/mangos/v2 v2.0.2
--- messaging/messaging.go
+++ messaging/messaging.go
@@ -25,7 +25,7 @@ type BaseMessage struct {
 
 //RegisterAgent is for agents to register with the server
 type RegisterAgent struct {
-	AgentID       int `storm:"id,unique"`
+	AgentID       int `storm:"id,increment"`
 	AgentHostName string
 	AgentIPAddr   string
 	AgentJoinDate time.Time
@@ -33,34 +33,43 @@ type RegisterAgent struct {
 
 //MessageDecode handles all incoming messages
 func MessageDecode(msg []byte) {
+	gob.Register(RegisterAgent{})
 	decodedMsg := BaseMessage{}
 	b := bytes.NewBuffer(msg)
-	gob.NewDecoder(b).Decode(&decodedMsg)
+	err := gob.NewDecoder(b).Decode(&decodedMsg)
+	if err != nil {
+		Logger.Error().Err(err).Msg("Unable to decode message!")
+	}
 	Logger.Debug().Msg("Message Received from Agent")
 	fmt.Printf("%+v\n", decodedMsg)
-	var messageBody map[string]interface{}
-	if decodedMsg.MessageBody != nil && decodedMsg.MessageBody != "" {
-		messageBody = decodedMsg.MessageBody.(map[string]interface{})
+	if decodedMsg.MessageBody == nil || decodedMsg.MessageBody == "" { //If the message body is missing or blank, return
+		Logger.Error().Msg("Unable to decode message from agent")
+		return
 	}
 	switch decodedMsg.MessageType {
 	case "RegisterAgent":
+		messageBody := decodedMsg.MessageBody.(RegisterAgent)
 		registerAgentHandler(messageBody)
 	}
 }
 
 //MessageEncode will encode and test the message (and maybe send it?)
-func MessageEncode(msg *BaseMessage) *bytes.Buffer {
+func MessageEncode(msg BaseMessage) []byte {
+	gob.Register(RegisterAgent{})
+	fmt.Printf("%+v\n", msg)
 	b := new(bytes.Buffer)
-	gob.NewEncoder(b).Encode(&msg)
-	return b
+	err := gob.NewEncoder(b).Encode(msg)
+	if err != nil {
+		Logger.Error().Err(err).Msg("Unable to encode message!")
+	}
+	Logger.Info().Msg("Message encoded")
+	return b.Bytes()
 }
 
-func registerAgentHandler(messageBody map[string]interface{}) {
-	newAgent := RegisterAgent{
-		messageBody["AgentID"].(int),
-		messageBody["AgentHostName"].(string),
-		messageBody["AgentIPAddr"].(string),
-		messageBody["AgentJoinDate"].(time.Time),
+func registerAgentHandler(newAgent RegisterAgent) {
+	fmt.Println("AgentHostName", newAgent.AgentHostName, "AgentIPAddr", newAgent.AgentIPAddr, "AgentJoinDate", newAgent.AgentJoinDate)
+	err := StormDB.Save(&newAgent)
+	if err != nil {
+		Logger.Error().Err(err).Msg("Unable to write to database!")
 	}
-	StormDB.Save(&newAgent)
 }
